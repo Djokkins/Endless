4772bb402bd11a1e54b233b20c84d140507cb978
Bertram <djokkins@gmail.com>
2023-11-05 19:46:53 +0100

    fixed y-home and bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1574,12 +1574,12 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define Y_MIN_POS -6
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
